6d7b2d2386bd0a47d7c7ae17bb8d3ba1b8e86568
Connor Campbell <ccampbell.james@gmail.com>
2024-09-24 12:28:56 +0100

    add dynamo db and s3 functions

---
+++ src/lib/s3.ts
@@ -0,0 +1,39 @@
+import { S3Client, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+
+const s3Client = new S3Client({ region: process.env.AWS_REGION, credentials: {accessKeyId : process.env.AWS_ACCESS_KEY!, secretAccessKey:process.env.AWS_SECRET_KEY! } });
+
+
+export async function uploadFileToS3(
+  accountAddress: string,
+  propertyId: number,
+  fileName: string,
+  fileType: string,
+  fileBody: Buffer | Blob
+): Promise<string> {
+  const fileKey = `${accountAddress}/${propertyId}/${fileName}`;
+
+  const params = {
+    Bucket: process.env.AWS_S3_BUCKET_NAME,
+    Key: fileKey,
+    Body: fileBody,
+    ContentType: fileType
+  };
+
+  const command = new PutObjectCommand(params);
+  await s3Client.send(command);
+
+  return fileKey;
+}
+
+export async function generatePresignedUrl(fileKey: string, expiresIn = 300): Promise<string> {
+    const command = new GetObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: fileKey,
+    });
+  
+    return getSignedUrl(s3Client, command, { expiresIn });
+  }
