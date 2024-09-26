084eae78f6b2ce70894c535aacf27789ec9fca20
izaakwalz <chukwumaizaak@gmail.com>
2024-09-26 16:19:31 +0100

    update APY

--- src/components/cards/market-card.tsx
+++ src/components/cards/market-card.tsx
@@ -67,9 +67,7 @@ export default function MarketCard({
             <dt className="text-md font-bold text-black">{metaData.property_name}</dt>
             <dd className="">
               APY{' '}
-              <span className="text-md font-bold text-black">
-                {parseInt(`${APY}`).toFixed(1)}%
-              </span>
+              <span className="text-md font-bold text-black">{formatNumber(APY * 10)}%</span>
             </dd>
           </div>
           <div className="flex items-center justify-between">
