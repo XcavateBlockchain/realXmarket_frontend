8892133a30125593a7b0d453554272faf723a10f
izaakwalz <chukwumaizaak@gmail.com>
2024-11-08 11:31:56 +0100

    update text-font

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
     <Shell className="gap-12">
       <div className="flex w-full flex-col items-center justify-center gap-10">
         <div className="flex w-full flex-col items-center justify-center gap-4">
-          <h1 className="bg-[linear-gradient(90deg,_#ecb278_-25.47%,_#dc7da6_35.16%,_#3b4f74_69.39%,_#57a0c5_103.47%)] bg-clip-text text-center font-mona font-black uppercase text-transparent md:text-[1.875rem]/[2.5rem] lg:text-[2.5rem]/[3.5rem]">
+          <h1 className="bg-[linear-gradient(90deg,_#ecb278_-25.47%,_#dc7da6_35.16%,_#3b4f74_69.39%,_#57a0c5_103.47%)] bg-clip-text text-center font-mona text-[1.5rem] font-black uppercase text-transparent md:text-[1.875rem]/[2.5rem] lg:text-[2.5rem]/[3.5rem]">
             REAL ESTATE INVESTMENT MADE SIMPLE
           </h1>
           <p className="text-balance text-center text-[16px]/[24px] md:text-[18px]/[24px] lg:text-[22px]/[24px]">
