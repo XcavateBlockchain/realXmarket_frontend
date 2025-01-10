0d2461e1dcf6eb2c52179465b78dd2d71a868c43
izaakwalz <chukwumaizaak@gmail.com>
2025-01-10 09:06:48 +0100

    fix: error when address not found

--- src/app/(app)/profile/layout.tsx
+++ src/app/(app)/profile/layout.tsx
@@ -5,6 +5,8 @@ import ProfileBannerImage from '@/components/profile-banner-image';
 import { getCookieStorage } from '@/lib/cookie-storage';
 import { profiles } from '@/config/profiles';
 import { getTokensAndListingsOwnedByAccount, getOnGoingObjectListing } from '@/lib/queries';
+import { Button } from '@/components/ui/button';
+import ConnectWalletButton from '@/components/wallet/connect-wallet';
 
 interface ProfileLayoutProps {
   children: React.ReactNode;
@@ -16,6 +18,15 @@ export default async function DeveloperLayout({ children }: Readonly<ProfileLayo
   const profile = profiles[address as string] ?? null;
 
   const tokensOwned: any = await getTokensAndListingsOwnedByAccount(address as string);
+
+  if (!address || !profile) {
+    return (
+      <div className="flex w-full flex-col items-center justify-center gap-6 py-20">
+        <p>Please Connect your wallet, and verify your credential.</p>
+        {!address ? <ConnectWalletButton /> : null}
+      </div>
+    );
+  }
 
   return (
     <>
