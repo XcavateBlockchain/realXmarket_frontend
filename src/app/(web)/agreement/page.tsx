7b16065ae1efdfbbe45f10bb96f185d2a5bf3fae
izaakwalz <chukwumaizaak@gmail.com>
2025-08-19 09:58:37 +0100

    chore:  add terms and conditions page

---
+++ src/app/(web)/agreement/page.tsx
@@ -0,0 +1,11 @@
+import { Shell } from '@/components/shell';
+
+export default function Agreement() {
+  return (
+    <Shell>
+      <div className={'flex w-full max-w-screen-lg flex-col items-start justify-center gap-6'}>
+        <h1 className=" text-3xl font-bold">Agreement</h1>
+      </div>
+    </Shell>
+  );
+}
---
+++ src/app/(web)/terms/page.tsx
@@ -0,0 +1,11 @@
+import { Shell } from '@/components/shell';
+
+export default function Terms() {
+  return (
+    <Shell>
+      <div className={'flex w-full max-w-screen-lg flex-col items-start justify-center gap-6'}>
+        <h1 className=" text-3xl font-bold">Terms of service</h1>
+      </div>
+    </Shell>
+  );
+}
